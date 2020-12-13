fc97e51971627475a000f3fc2bd8162692938991
Tobias Klumpp <tobias.klumpp@toklumpp.net>
2020-12-13 13:36:50 +0100

    Renamed compass to index, started with structure

--- index.js
+++ index.js
@@ -1,16 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App.js';
-import reportWebVitals from './reportWebVitals';
+document.addEventListener('DOMContentLoaded', function(event) {
 
-ReactDOM.render(
-  <React.StrictMode>
-  <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  if (window.DeviceOrientationEvent) {
+    document.getElementById('notice').innerHTML = 'Working API detected';
+    window.addEventListener('deviceorientation', (eventData) => {
+      // gamma: Tilting the device from left to right. Tilting the device to the right will result in a positive value.
+      const tiltLR = eventData.gamma;
+      // beta: Tilting the device from the front to the back. Tilting the device to the front will result in a positive value.
+      const tiltFB = eventData.beta;
+      // alpha: The direction the compass of the device aims to in degrees.
+      const dir = eventData.alpha
+      // Call the function to use the data on the page.
+      deviceOrientationHandler(tiltLR, tiltFB, dir);
+    }, false);
+  } else {
+    document.getElementById('notice').innerHTML = 'No API detected'
+  };
+
+  function deviceOrientationHandler(tiltLR, tiltFB, dir) {
+    document.getElementById('tiltLR').innerHTML = Math.ceil(tiltLR);
+    document.getElementById('tiltFB').innerHTML = Math.ceil(tiltFB);
+    document.getElementById('direction').innerHTML = Math.ceil(dir);
+    // Rotate the disc of the compass. - CSS transform
+    const compassDisc = document.getElementById('compassDiscImg');
+    compassDisc.style.transform = `rotate(${dir}deg)`;
+    compassDisc.style.webkitTransform = `rotate(${dir}deg)`;
+    compassDisc.style.MozTransform = `rotate(${dir}deg)`;
+  }
+
+});
