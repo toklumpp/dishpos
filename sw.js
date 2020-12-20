319dbe4b8260e5baabe1eaf371234cf65e04a27f
Tobias Klumpp <tobias.klumpp@toklumpp.net>
2020-12-20 21:56:00 +0100

    Updated impressum and privacy policy

--- sw.js
+++ sw.js
@@ -14,13 +14,14 @@
 // Names of the two caches used in this version of the service worker.
 // Change to v2, etc. when you update any of the local resources, which will
 // in turn trigger the install event again.
-const PRECACHE = 'precache-v7';
+const PRECACHE = 'precache-v8';
 const RUNTIME = 'runtime';
 
 // A list of local resources we always want to be cached.
 const PRECACHE_URLS = [
   'index.html',
-  'impressum.html',
+  'legal_de.html',
+  'legal_en.html',
   './', // Alias for index.html
   'style.css',
   'icon.svg',
